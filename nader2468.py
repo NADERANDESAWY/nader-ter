2d269ac0fd6f27c802ee1b566311a15b7a9b18c4
NADERANDESAWY <129998985+NADERANDESAWY@users.noreply.github.com>
2023-04-05 23:03:50 +0200

    Add files via upload

---
+++ nader2468.py
@@ -0,0 +1,8 @@
+import os
+print ("\033[1;36m nader \033[1;34m and \033[1;36m fareed \033[1;32m ")
+os.system ('clear')
+
+ucybers00 = 10000000
+while ucybers00 < 99999999 :
+    ucybers00 = ucybers00 + 1
+    print (ucybers00)
