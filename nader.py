f3e6d4b486b19b44937668ad1c34124282149aa5
NADERANDESAWY <129998985+NADERANDESAWY@users.noreply.github.com>
2023-04-06 21:24:16 +0200

    Add files via upload

---
+++ nader.py
@@ -0,0 +1,12 @@
+import os
+import webbrowser
+os.system("clear")
+print ("                    \033[1;36m nader \033[1;34m and \033[0;31m fareed \033[1;32m                  ")
+Name = input("Enter Name : ")
+Name2 = input("Enter First Name : ")
+Password = input("Enter Password : ")
+print (Name)
+print(Name2)
+print (Password)
+sear = input(Name  +  " Enter Search Here : ")
+webbrowser.open("https://www.google.com/search?q=" + sear)
